e1417ef70be39bf6bd1fdbbac3d4202d81050dc7
pawan-panth <pawan.panth.pp@gmail.com>
2021-01-03 15:19:57 +0530

    m

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,109 @@
-import React, {Component, useState} from "react";
+import React, {Component, useEffect, useRef, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const [Time, setTime] = useState("25");
+  const [Minute, setMinute] = useState(25);
+  const [Seconds, setSeconds] = useState(0);
+  const [Break,setBreak] = useState(5);
+  const [Work,setWork] = useState(25);
+  const [started,setStarted] = useState(false);
+  let [timer,setTimer] = useState("");
+  //const timer = useRef(null);//refer to the initial state and is access to every other rerenders;
+  const startHandler =(evt) => {
+    setStarted(!started);
+    timer = setInterval(()=>setTimer1(),1000);
+    //setTimer(setInterval(()=>setTimer1(),1000));//other way of doing it if timer state is const
+    //timer.current = setInterval(()=>setTimer(),1000);//if you are using useref;
+    setTimer(timer);
+  }
+  let numSeconds = Seconds;
+  let numMinutes = Minute;
+  const setTimer1 = () =>{
+    if(numSeconds === 0 && numMinutes === 0){
+      alert("work duration is over");
+      clearInterval(timer);
+      setMinute(Break);
+      setSeconds(0);
+      numMinutes = Break;
+      numSeconds = 0;
+      setTimer2();
+      return;
+    }
+    if(numSeconds === 0){
+      setMinute(numMinutes - 1);
+      numMinutes -= 1;
+      numSeconds = 60;
+    }
+    setSeconds(numSeconds-1);
+    numSeconds = numSeconds-1;  
+  }
+  const setTimer2 = () =>{
+    timer = setInterval(()=>Timer2(),1000);
+    setTimer(timer);
+  }
+  const Timer2 =()=>{
+    if(numSeconds === 0 && numMinutes === 0){
+      alert("break duration is over");
+      clearInterval(timer);
+      setMinute(Work);
+      setSeconds(0);
+      numMinutes = Work;
+      numSeconds = 0;
+      setTimer2();
+      return;
+    }
+    if(numSeconds === 0){
+      setMinute(numMinutes - 1);
+      numMinutes -= 1;
+      numSeconds = 60;
+    }
+    setSeconds(numSeconds-1);
+    numSeconds = numSeconds-1; 
+  }
+  const stopHandler = () => {
+    // clearInterval(timer.current);
+    setStarted(!started);
+    clearInterval(timer);
+
+  }
+  const setHandler =()=>{
+    setWork(Work);
+    setBreak(Break);
+    setMinute(Work);
+    setSeconds(0);
+    
+  }
+  const reset =() =>{
+    clearInterval(timer);
+    setTimer("");
+    setStarted(false);
+    setMinute(25);
+    setSeconds(0);
+    setWork(25);
+    setBreak(5);
+    
+
+  }
+
   return (
-    <div id="main"></div>
+    <div id="main">
+      
+      <h1>{Minute<10?"0"+Minute:Minute}:{Seconds<10?"0"+Seconds:Seconds}</h1>
+      <h1>Work Time</h1>
+      <div className="btn">
+        <button data-testid='start-btn' onClick={startHandler} disabled={started?true:false}>start</button>
+        <button data-testid='stop-btn' onClick={stopHandler} disabled={started?false:true}>stop</button>
+        <button data-testid='reset-btn' onClick={reset}> Reset</button>
+      </div>
+      <div className="btn">
+        <input type="Number" onChange={(evt)=>setWork(evt.target.value)} value={Work} disabled={started?true:false}></input>
+        <input type="Number" onChange={(evt)=>setBreak(evt.target.value)} value={Break} disabled={started?true:false}></input>
+        <button onClick={setHandler} disabled={started?true:false}>set</button>
+      </div>
+      
+    </div>
   )
 }
-
 
 export default App;
